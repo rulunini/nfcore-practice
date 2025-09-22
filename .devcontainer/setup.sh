ee545ce03aeb23e03893d53ac35c432cc7f2ac13
rulunini <sohee5172@gmail.com>
2025-09-23 06:19:43 +0900

    upload all

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,12 +7,4 @@ export PS1='\[\e[3;36m\]${PWD#/workspaces/} ->\[\e[0m\] '
 # Install vim
 apt-get update && apt-get install -y vim
 
-# Install Nextflow
-curl -s https://get.nextflow.io | bash
-mv nextflow /usr/local/bin/
-chmod +x /usr/local/bin/nextflow
-
-# Install Java (required for Nextflow)
-apt-get install -y openjdk-11-jdk
-
 cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt
